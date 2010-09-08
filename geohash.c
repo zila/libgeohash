7ebf5d1ab107471cea3e6230a3c272b637647bb4
Andrey Subbotin <andrey@subbotin.me>
2010-09-08 14:17:08 -0400

    The string in the first parameter of strncat must have sufficient space to hold the result.

--- geohash.c
+++ geohash.c
@@ -83,7 +83,7 @@ char* get_neighbor(char *hash, int direction) {
     char **border = is_odd ? odd_borders : even_borders;
     char **neighbor = is_odd ? odd_neighbors : even_neighbors; 
     
-    char *base = malloc(sizeof(char) * 1);
+    char *base = malloc(sizeof(char) * hash_length);
     base[0] = '\0';
     strncat(base, hash, hash_length - 1);
     
